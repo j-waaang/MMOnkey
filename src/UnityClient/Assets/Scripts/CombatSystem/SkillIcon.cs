1537e9be954b6999c511103fedcf12433b2694e3
Jing Yi Wang <jingyilogin@outlook.de>
2017-05-20 15:47:45 +0200

    Added skill icons, skillbar and target bar

---
+++ src/UnityClient/Assets/Scripts/CombatSystem/SkillIcon.cs
@@ -0,0 +1,29 @@
+﻿namespace JYW.ThesisMMO.UnityClient.CombatSystem {
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class SkillIcon : MonoBehaviour {
+
+        [SerializeField]
+        private string m_Skillname;
+
+        internal void ChangeSkill(string skillname) {
+            m_Skillname = skillname;
+            LoadSkill();
+        }
+
+        private void Awake() {
+            LoadSkill();
+        }
+
+        private void LoadSkill() {
+            var image = GetSkillIcon(m_Skillname);
+            GetComponent<Image>().sprite = image;
+            gameObject.name = m_Skillname;
+        }
+
+        private static Sprite GetSkillIcon(string skillName) {
+            return Resources.Load<Sprite>(skillName);
+        }
+    }
+}
