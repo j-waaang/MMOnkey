c5ba65445c6a473a80e380dffd97999691ee98df
Jing Yi Wang <jingyilogin@outlook.de>
2017-07-04 14:44:43 +0200

    Close #18. Implemented HammerBash Skill

---
+++ src/MMOServer/ActionObjects/SkillRequests/HammerBashRequest.cs
@@ -0,0 +1,40 @@
+﻿namespace JYW.ThesisMMO.MMOServer.ActionObjects.SkillRequests {
+
+    using Photon.SocketServer;
+
+    using Entities.Attributes.Modifiers;
+    using Common.Codes;
+    using Common.ContinueObjects;
+    using Common.Types;
+    using Targets;
+
+    internal class HammerBashRequest : CastActionObject {
+
+        public HammerBashRequest(string actionSource, IRpcProtocol protocol, OperationRequest request)
+            : base(actionSource, protocol, request) {
+        }
+
+        private const float RADIUS = 2.75f;
+
+        public override void StartAction() {
+            FinishedCastingEvent += DoDamage;
+            FinishedCastingEvent += SetIdle;
+            StartCast(new System.TimeSpan(0, 0, 0, 1), new Vector(0, -1f));
+        }
+
+        private void DoDamage(CallReason continueReason) {
+            var healthModifier = new IntModifier(ModifyMode.Addition, AttributeCode.Health, -7);
+            var sourcePos = World.Instance.GetEntity(ActionSource).Position;
+
+            var dmgArea = new CircleAreaTarget(sourcePos, RADIUS) {
+                AreaTargetOption = AreaTargetOption.IgnoreSource,
+                SourceName = ActionSource
+            };
+
+            World.Instance.ApplyModifier(dmgArea, healthModifier);
+
+            var interupted = World.Instance.GetEntitesInArea(dmgArea);
+            m_InteruptionHandler.OnInterupt(interupted);
+        }
+    }
+}
