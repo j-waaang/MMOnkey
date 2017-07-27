3b23d6fb0e78e5405fe127e4798e9cbc081c343a
Jing Yi Wang <jingyilogin@outlook.de>
2017-07-28 00:15:00 +0200

    Close #26. Client region hints

---
+++ src/UnityClient/Assets/Scripts/Util/RegionHintCreator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RegionHintCreator : MonoBehaviour {
+
+    [SerializeField] private GameObject m_HintPrefab;
+    private static readonly Vector3 m_Start = new Vector3(-45F, 0.5F, -45F);
+    private const float Offset = 10F;
+    private const int Dimension = 10;
+
+	private void Awake () {
+		for(var x = 0; x < Offset; ++x) {
+            for(var z = 0; z < Offset; z++) {
+                var pos = m_Start + Vector3.right * x * Offset + Vector3.forward * z * Offset;
+                Instantiate(m_HintPrefab, transform).transform.position = pos;
+            }
+        }
+	}
+}
