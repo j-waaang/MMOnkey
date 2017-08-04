705cb1351eb6534101d3a34079399135d72ecb6b
Jing Yi Wang <jingyilogin@outlook.de>
2017-08-04 15:49:53 +0200

    Target Skills has high consitency at max range. Close #32

--- src/MMOServer/Skills/OrisonOfHealingData.cs
+++ src/MMOServer/Skills/OrisonOfHealingData.cs
@@ -1,9 +1,15 @@
-﻿namespace JYW.ThesisMMO.MMOServer.Skills {
+﻿using JYW.ThesisMMO.MMOServer.CSAIM;
+
+namespace JYW.ThesisMMO.MMOServer.Skills {
     class OrisonOfHealingData : SkillData {
         public override float MaxRange {
             get {
                 return 8F;
             }
+        }
+
+        public override MsInInterval GetConsistencyRequirement() {
+            return new MsInInterval(MaxRange-1F, MaxRange+1F, 0);
         }
     }
 }
